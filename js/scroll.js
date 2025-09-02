a338c9909a779a0a09706dca3bf2a603a5c8322c
HananBAli <hananbmansour@gmail.com>
2025-09-02 18:44:46 +0200

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+﻿$(window).on("load", function () {
+
+    var up = $('#upButton');
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() >= 200) {
+            up.fadeIn(500);
+            //$('#upButton').fadeIn(1000);
+        }
+        else {
+            up.fadeOut(500);
+        }
+
+    });
+
+    up.on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 10);
+    });
+});
